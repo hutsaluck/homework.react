23c2647418b435d6b385591dbaae2b27a827a6e8
Юра Гуцалюк <hutsaluck@gmail.com>
2025-01-12 22:40:20 +0200

    Styling project

--- src/components/character-component/CharacterComponent.tsx
+++ src/components/character-component/CharacterComponent.tsx
@@ -8,10 +8,21 @@ interface ICharacterComponentProps {
 
 export const CharacterComponent = ({item, children}: ICharacterComponentProps) => {
     return (
-        <div className="my-10 border-2 border-gray-200">
-            <h3 className="text-2xl">{item.name} {item.surname}</h3>
-            <img src={item.photo} alt={item.name} />
-            <p>{children}</p>
+        <div
+            className="my-10 border border-gray-300 rounded-2xl grid justify-center items-center cursor-pointer transition-shadow duration-500 hover:shadow-lg">
+            <div className="overflow-hidden rounded-t-2xl h-64">
+                <img
+                    src={item.photo}
+                    alt={item.name}
+                    className="w-full h-full object-contain rounded-t-2xl transition-transform duration-500 hover:scale-110"
+                />
+            </div>
+            <div className="p-4">
+                <h3 className="text-2xl font-semibold text-center mb-2">
+                    {item.name} {item.surname}
+                </h3>
+                <p className="text-center max-h-[160px] hover:max-h-full overflow-hidden text-ellipsis transition-all duration-300">{children}</p>
+            </div>
         </div>
     );
 };
--- src/components/family-component/FamilyComponent.tsx
+++ src/components/family-component/FamilyComponent.tsx
@@ -3,7 +3,7 @@ import {CharacterComponent} from "../character-component/CharacterComponent.tsx"
 
 export const FamilyComponent = () => {
     return (
-        <div>
+        <div className="grid grid-cols-5 gap-5 justify-center items-start mx-5">
             {
                 simpsons.map((value, index) => <CharacterComponent item={value} key={index}>
                     {value.info}
