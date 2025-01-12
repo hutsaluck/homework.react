4daad582bcd6c7c488b8928ff629a9012f0aa4d1
Юра Гуцалюк <hutsaluck@gmail.com>
2025-01-12 23:38:26 +0200

    Styling project

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,8 +7,24 @@ interface TodoProps {
 export const Todo = ({item}: TodoProps) => {
     return (
         <div
-            className="my-10 border border-gray-300 rounded-2xl grid justify-center items-center cursor-pointer transition-shadow duration-500 hover:shadow-lg">
+            className={`p-4 my-10 border rounded-2xl grid justify-center items-center cursor-pointer transition-shadow duration-500 hover:shadow-lg ${item.completed ? 'border-green-500 bg-green-100' : 'border-gray-300 bg-gray-100'}`}>
             <h3 className="text-2xl font-semibold text-center mb-2">{item.title}</h3>
+
+            <div className="flex items-center justify-center space-x-2 mt-2">
+                {item.completed ? (
+                    <>
+                        <span className="text-green-500">✔️</span>
+                        <span className="text-green-500">Зроблено</span>
+                    </>
+                ) : (
+                    <>
+                        <span className="text-yellow-500">⌛</span>
+                        <span className="text-yellow-500">В очікуванні</span>
+                    </>
+                )}
+            </div>
         </div>
+
+
     );
 };
