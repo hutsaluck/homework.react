5e4aea0c15b7274f61776893a8e25706487e3e82
Юра Гуцалюк <hutsaluck@gmail.com>
2025-01-15 15:56:53 +0200

    Base route structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {RouterProvider} from "react-router";
+import {routes} from "./router/router.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<RouterProvider router={routes} />)
---
+++ src/router/router.tsx
@@ -0,0 +1,9 @@
+import {createBrowserRouter} from "react-router";
+import {MainLayout} from "../layouts/MainLayout.tsx";
+
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout/>,
+    }
+])
